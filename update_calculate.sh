b4aa8b0a4e23b8dc26a1d9716bf70d2701b072fa
marav <maravtdm@users.noreply.github.com>
2020-09-01 15:22:21 +0200

    Update update_calculate.sh

--- update_calculate.sh
+++ update_calculate.sh
@@ -4,7 +4,7 @@ do
   cl-update -p > /tmp/update_result.txt
   maj=$(cat /tmp/update_result.txt | awk '/à télécharger/ {print $2}')
   if [ ! -z "${maj}" ] && [ "${maj}" != "0" ]; then
-    notify-send -u critical "cl-update : $maj mise(s) à jour disponible(s)"
+    notify-send -u critical "cl-update : ${maj} mise(s) à jour disponible(s)"
     exit 0
   fi
 sleep 86353
